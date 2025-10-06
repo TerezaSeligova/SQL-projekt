fa2fbfb4e167377fa90338c5844a5f65e35ff0aa
TerezaSeligova <terezaseligova@icloud.com>
2025-10-06 15:47:03 +0200

    Update and rename Spojení_tabulek.sql to 1.otázka.sql

---
+++ 1.otázka.sql
@@ -0,0 +1,51 @@
+WITH cte_prepare AS (
+		SELECT 
+			date_from,
+			AVG(avg_wage) AS avg_wage,
+			sector_name 
+		FROM t_tereza_seligova_project_sql_primary_final
+		GROUP BY date_from, sector_name
+),
+	cte_previous_wage AS ( 
+		SELECT 
+			date_from,
+			avg_wage,
+			LAG(avg_wage) OVER (Partition BY sector_name ORDER BY date_from) AS previous_wage,
+			sector_name
+		FROM cte_prepare
+),
+	cte_diff AS (
+		SELECT 
+			date_from, 
+			avg_wage, 
+			previous_wage,
+			avg_wage - previous_wage AS difference,
+			sector_name
+		FROM cte_previous_wage
+),
+	cte_result AS ( 
+		SELECT 
+			date_from,
+			sector_name,
+			CASE WHEN difference > 0 THEN 'INCREASING'
+				WHEN difference < 0 THEN 'DECREASING'
+				ELSE 'STAGNACE' 
+				END AS outcome
+		FROM cte_diff
+)
+SELECT * 
+FROM cte_result
+WHERE outcome = 'DECREASING'
+ORDER BY date_from;
+			
+		--- v některých odvětvích mzdy klesají meziročně klesají
+
+
+		
+	
+	
+		
+		
+	
+
+
